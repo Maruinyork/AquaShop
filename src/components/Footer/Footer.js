c6f6c8e7b092839e591a332eb1bec5506ec3b414
maruinyork <almadebudin23@hotmail.com>
2022-09-07 01:14:10 -0300

    modificacion de footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Logo from '../../assets/logo2.png'
+import AppStore from '../../assets/app.jpg'
+import GooglePlay from '../../assets/play.jpg'
+import Pay from '../../assets/pay.png'
 import '../Footer/Footer.css'
 
 const Footer = () => {
@@ -9,11 +12,8 @@ const Footer = () => {
       <footer className="text-white bg-black py-3">
         <div className="container">
           <nav className="row">
-            <Link
-              to="/"
-              className=" column col-12 col-md-3"
-            >
-              <img src={Logo} alt="" height="35" width='100' />
+            <Link to="/" className=" column col-12 col-md-3">
+              <img src={Logo} alt="" height="35" width="100" />
             </Link>
             <ul className=" column col-12 col-md-3 list-unstyled">
               <li className="fw-bold">E-commerce</li>
@@ -50,10 +50,22 @@ const Footer = () => {
                   <i className=" textFooter bi bi-twitter" />
                 </a>
               </li>
+              <div className="install">
+                <h5> Instalar App</h5>
+                <p>Desde App Store o Google Play</p>
+                <img src={AppStore} alt="" />
+                <img src={GooglePlay} alt="" />
+                <h5>Medios de pago seguros</h5>
+                <img src={Pay} alt=""  className='pay'/>
+              </div>
             </ul>
+
             <div>
-              <p className='text-center'>
-                <i className="bi bi-c-circle"> 2022 Maruinyork | All right reserved</i>
+              <p className="text-center">
+                <i className="bi bi-c-circle">
+                  {' '}
+                  2022 Maruinyork | All right reserved
+                </i>
               </p>
             </div>
           </nav>
